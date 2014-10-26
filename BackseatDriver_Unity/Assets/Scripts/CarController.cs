c07cb61ecd0225a05c7d56cc8642d86bf859194e
ben <changb3@rpi.edu>
2014-10-26 13:54:31 -0400

    Starting point for player driving control.  Player prefab is edited in CarControls.unity. WASD to drive.  Has parameters to control speed, turn speed, and antiSlip.  Anti-Slip applies a force along the car's local X-axis to prevent it from sliding sideways when turning.

---
+++ BackseatDriver_Unity/Assets/Scripts/CarController.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CarController : MonoBehaviour {
+
+	float throttle=0f;
+	float steer=0f;
+	public float turnSpeed = 30;
+	public float speed = 160; 
+	public float antiSlip = 100.0f;
+	void Start () {
+		Debug.Log (transform.forward);
+	}
+	
+	void Update () {
+		GetInput ();
+	}
+
+	// use this for rigidbody physics updates
+	void FixedUpdate () {
+
+		ApplyFriction ();
+		ApplyThrottle ();
+		ApplySteering ();
+
+	}
+
+	void GetInput() {
+		throttle = Input.GetAxis ("Vertical");
+		steer = Input.GetAxis ("Horizontal");
+
+	}
+
+	// 
+	void ApplyFriction ()
+	{
+		Vector3 relativeVelocity = transform.InverseTransformDirection(rigidbody.velocity);
+
+		// apply sideways friction to prevent slipping
+
+		float sqrVel = relativeVelocity.x * relativeVelocity.x;
+
+
+		Vector3 antiSlipVecLocal = sqrVel * Vector3.right * Mathf.Sign (relativeVelocity.x) * antiSlip * -1;
+
+		Vector3 antiSlipVec = transform.TransformDirection (antiSlipVecLocal);
+
+		rigidbody.AddForce (antiSlipVec  * Time.fixedDeltaTime);
+	}
+
+	void ApplyThrottle ()
+	{
+		rigidbody.AddForce (transform.forward * Time.fixedDeltaTime * throttle * speed );
+	}
+
+	void ApplySteering ()
+	{
+		transform.RotateAround (transform.position, Vector3.up, steer * Time.fixedDeltaTime * turnSpeed);
+	}
+
+}
