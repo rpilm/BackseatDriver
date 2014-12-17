afb51a9115c5d834a0a85d2da387e34496ec5fbf
Alexa Aranjo <aranja@rpi.edu>
2014-12-16 19:53:00 -0800

    Hopefully, all the files for real.

---
+++ BackseatDriver_Unity/Assets/Scripts/Path.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using System.Collections;
+using Pathfinding;
+using System.Collections.Generic;
+
+namespace Pathfinding {
+	
+	public abstract class Path {
+		
+		public PathFinder pathFinder;
+		
+		
+		private GraphNode[] aPath;
+		private Vector3[] aVectorPath;
+		
+		public List<GraphNode> path;
+		
+		public List<Vector3> vectorPath;
+		
+		public float maxFrameTime;
+		
+		protected PathNode current;
+		
+		public float duration;			
+		public int searchIterations = 0;
+		public int searchedNodes;		
+		
+		
+		
+		
+		public float GetTotalLength () {
+			if (vectorPath == null) return float.PositiveInfinity;
+			float tot = 0;
+			for (int i=0;i<vectorPath.Count-1;i++) tot += Vector3.Distance (vectorPath[i],vectorPath[i+1]);
+			return tot;
+		}
+		
+	}
