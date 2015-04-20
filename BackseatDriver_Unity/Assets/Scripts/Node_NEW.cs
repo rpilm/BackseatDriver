e8541ae5473856cda30f152e0b79ee34d71b7793
Karl Olsen <droodftw123@gmail.com>
2015-04-20 16:38:25 -0400

    Node work (still heavily WIP)

---
+++ BackseatDriver_Unity/Assets/Scripts/Node_NEW.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Node_NEW : MonoBehaviour {
+	public int num_neighbors;
+	public GameObject neighbors;
+
+	void OnTriggerEnter(Collider c)	{
+		if (c.gameObject.name == "Player") {
+			return;
+
+		}
+		return;
+	}
+
+	void OnTriggerExit(Collider c)	{
+		if (c.gameObject.name == "Player") {
+			//****************************************************
+			//NEED TO CHANGE THE FIRST ARGUMENT TO THE TARGET_NODE
+			//****************************************************
+			float dot = Vector3.Dot(this.gameObject.transform.rotation.eulerAngles, c.gameObject.transform.rotation.eulerAngles);
+			if(dot > 0.8f)	{
+				Debug.Log ("WOO YOU MADE THE CORRECT TURN!");
+			}
+			else {
+				Debug.Log ("Learn how to drive");
+			}
+		}
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
