c6e8dc09d730b13610bea4b6ed5fa60440104a24
Karl Olsen <droodftw123@gmail.com>
2015-05-16 15:25:56 -0400

    Added in debug.log messages saying  to "turn left" "turn right" or "straight" in addition to the reactions. REMAINING TASKS: -code what to do from going from "final" node to the actual target. -clean up code a bit -finish commenting up code and write a README.txt -maybe clean up driving code (with the "sliding", it can cause bugs with the pathfinding code)

--- BackseatDriver_Unity/Assets/Scripts/Node_NEW.cs
+++ BackseatDriver_Unity/Assets/Scripts/Node_NEW.cs
@@ -9,20 +9,35 @@ public class Node_NEW : MonoBehaviour {
 		if (c.gameObject.name == "Player") {
 			player.current_node = this.gameObject;
 			player.next_node = getNextNode();
+			player.tellNextDirection();
+			//**************************************************************
+			//TELL PLAYER WHICH DIRECTION TO GO HERE (left, right, straight)
+			//**************************************************************
+
 		}
 	}
-	
+
+
+	Vector3 correct_direction;
+	Vector3 player_direction;
+
+
 	void OnTriggerExit(Collider c)	{
 		if (c.gameObject.name == "Player") {
-			//****************************************************
-			//NEED TO CHANGE THE FIRST ARGUMENT TO THE TARGET_NODE
-			//****************************************************
-			float dot = Vector3.Dot(this.gameObject.transform.rotation.eulerAngles, c.gameObject.transform.rotation.eulerAngles);
-			if(dot > 0.8f)	{
-				Debug.Log ("WOO YOU MADE THE CORRECT TURN!");
-			}
-			else {
-				Debug.Log ("Learn how to drive");
+			correct_direction = player.next_node.transform.position - player.current_node.transform.position;
+			correct_direction.Normalize();
+			player_direction = player.gameObject.transform.forward;
+			player_direction.Normalize();
+			float dot = Vector3.Dot(correct_direction, player_direction);
+
+			//because corners are effectively one direction, they should not be reacted to
+			if(this.gameObject.name != "corner")	{
+				if(dot > 0.8f)	{
+					Debug.Log ("WOO YOU MADE THE CORRECT TURN!");
+				}
+				else {
+					Debug.Log ("Learn how to drive");
+				}
 			}
 		}
 	}
@@ -32,11 +47,29 @@ public class Node_NEW : MonoBehaviour {
 		float closest_dist = 100000f;
 		//just giving the first neighbor at first for the sake of initialization
 		GameObject pref_node = neighbors [0];
+
+		int player_dir = player.getDirection ();
+
 		for (int n = 0; n < neighbors.Length; n++) {
-			float current_dist = Vector3.Distance(player.target.transform.position, neighbors[n].transform.position);
-			if(current_dist < closest_dist)	{
-				pref_node = neighbors[n];
-				closest_dist = current_dist;
+			//the next node should NOT be the node that the player just came from (in case of player trying to U-turn or anything else)
+			if(player.dir == 0 && neighbors[n].transform.position.z < this.gameObject.transform.position.z)	{
+				continue;
+			}
+			else if(player.dir == 1 && neighbors[n].transform.position.x < this.gameObject.transform.position.x)	{
+				continue;
+			}
+			else if(player.dir == 2 && neighbors[n].transform.position.z > this.gameObject.transform.position.z)	{
+				continue;
+			}
+			else if(player.dir == 3 && neighbors[n].transform.position.x > this.gameObject.transform.position.x)	{
+				continue;
+			}
+			else {
+				float current_dist = Vector3.Distance(player.target.transform.position, neighbors[n].transform.position);
+				if(current_dist < closest_dist)	{
+					pref_node = neighbors[n];
+					closest_dist = current_dist;
+				}
 			}
 		}
 		return pref_node;
--- BackseatDriver_Unity/Assets/Scripts/Pathfinder_NEW.cs
+++ BackseatDriver_Unity/Assets/Scripts/Pathfinder_NEW.cs
@@ -7,13 +7,97 @@ public class Pathfinder_NEW : MonoBehaviour {
 	public GameObject next_node;
 	public GameObject target;
 
+	//direction tracking variable
+	//Beacuse y axis is irrelevant:
+	//0 = "up" (positive z)
+	//1 = "right" (positive x)
+	//2 = "down" (negative z)
+	//3 = "left" (negative x)
+	public int dir;
+
+
 	// Use this for initialization
 	void Start () {
 		current = current_node.GetComponent<Node_NEW> ();
+		dir = 0;
+	}
+
+	//see "
+	public int getDirection()	{
+		if (this.gameObject.transform.rotation.eulerAngles.y > 315)
+			dir = 0;
+		else
+			dir = (int)(this.gameObject.transform.rotation.eulerAngles.y / 90);
+
+		return dir;
+	}
+
+	public void tellNextDirection()	{
+		//if player arrives at the intersection moving up
+		if (dir == 0) {
+			if(next_node.transform.position.z > current_node.transform.position.z)	{
+				//tell the player to go straight
+				Debug.Log("STRAIGHT");
+			}
+			else if(next_node.transform.position.x > current_node.transform.position.x)	{
+				//tell the player to go right
+				Debug.Log("TURN RIGHT");
+			}
+			else if(next_node.transform.position.x < current_node.transform.position.x)	{
+				//tell the player to go left
+				Debug.Log("TURN LEFT");
+			}
+		}
+		//if player arrives at the intersection moving right
+		else if (dir == 1) {
+			if(next_node.transform.position.z > current_node.transform.position.z)	{
+				//tell the player to go left
+				Debug.Log("TURN LEFT");
+			}
+			else if(next_node.transform.position.z < current_node.transform.position.z)	{
+				//tell the player to go right
+				Debug.Log("TURN RIGHT");
+			}
+			else if(next_node.transform.position.x > current_node.transform.position.x)	{
+				//tell the player to go straight
+				Debug.Log("STRAIGHT");
+			}
+		}
+		//if player arrives at the intersection moving down  
+		else if (dir == 2) {
+			if(next_node.transform.position.z < current_node.transform.position.z)	{
+				//tell the player to go straight
+				Debug.Log("STRAIGHT");
+			}
+			else if(next_node.transform.position.x < current_node.transform.position.x)	{
+				//tell the player to go right
+				Debug.Log("TURN RIGHT");
+			}
+			else if(next_node.transform.position.x > current_node.transform.position.x)	{
+				//tell the player to go left
+				Debug.Log("TURN LEFT");
+			}
+		}
+		//if player arrives at the intersection moving left
+		else {
+			if(next_node.transform.position.z < current_node.transform.position.z)	{
+				//tell the player to go left
+				Debug.Log("TURN LEFT");
+			}
+			else if(next_node.transform.position.z > current_node.transform.position.z)	{
+				//tell the player to go right
+				Debug.Log("TURN RIGHT");
+			}
+			else if(next_node.transform.position.x < current_node.transform.position.x)	{
+				//tell the player to go straight
+				Debug.Log("STRAIGHT");
+			}
+		}
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+
 	}
 }
